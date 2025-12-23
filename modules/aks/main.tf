bea60fb0b4fa0c8c2f8f27f1ceb6ed10bc4cd426
baksoum <99664268+baksoum@users.noreply.github.com>
2025-12-23 18:48:14 -0500

    Add Azure Kubernetes Service configuration

---
+++ modules/aks/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+    subnet_id      = var.vnet_subnet_id
+  }
+}

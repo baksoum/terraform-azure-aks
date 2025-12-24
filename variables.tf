583d9d565fba00519da3c633882f08ef70be3691
baksoum <99664268+baksoum@users.noreply.github.com>
2025-12-23 19:06:40 -0500

    Add variables for resource group and AKS settings
    Define variables for Azure resource configuration

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "cluster_name" {
+  description = "AKS cluster name"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Number of nodes in the default node pool"
+  type        = number
+  default     = 2
+}
+
+variable "node_vm_size" {
+  description = "VM size for nodes"
+  type        = string
+  default     = "Standard_DS2_v2"
+}
+
+variable "dns_prefix" {
+  description = "DNS prefix for AKS cluster"
+  type        = string
+}
